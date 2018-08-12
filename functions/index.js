f9b113fa459e325dfef82704f4540a6405d4312e
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-08-13 08:03:58 +0900

    add function

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const profsRef = functions.database.ref('/profs/{uid}');
+const logsRef = admin.database().ref('/logs');
+
+const ADMIN_NAME = "■管理人■";
+const ADMIN_COLOR = "yellow";
+
+exports.postWeclomeMessage = profsRef.onCreate((snap, context) => {
+  console.debug('--- profs#onCreate')
+  const prof = snap.val();
+  return logsRef.push({
+    name: ADMIN_NAME,
+    color: ADMIN_COLOR,
+    body: `${prof.name || 'No Name'}さんが入室しました。`,
+    date: (new Date()).toISOString()
+  });
+});
+
+exports.postByeMessage = functions.auth.user().onDelete(user => {
+  console.debug('--- user#onDelete')
+  return admin.database().ref(`/profs/${user.uid}`).on('value', snap => {
+    const prof = snap.val();
+    logsRef.push({
+      name: ADMIN_NAME,
+      color: ADMIN_COLOR,
+      body: `${prof.name || 'No Name'}さんが退室しました。`,
+      date: (new Date()).toISOString()
+    })
+    return snap.remove();
+  })
+});
+
