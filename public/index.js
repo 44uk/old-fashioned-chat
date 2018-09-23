1e11b6b4dcbee885b6780f947480a01a01b742da
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-09-23 09:35:26 +0900

    update

--- public/index.js
+++ public/index.js
@@ -209,3 +209,14 @@ function main() {
   });
 }
 window.addEventListener('DOMContentLoaded', main);
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/serviceWorker.js")
+      .then(function(registration) {
+        console.log("serviceWorker registered.");
+      }).catch(function(error) {
+        console.warn("serviceWorker error.", error);
+      })
+    ;
+  }
+});
